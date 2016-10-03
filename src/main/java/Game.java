db378ce6091abdf8026cd81bb08d8881f9b9a440
Chance Neff and Satchel Grant <chance+satchel@>
2016-10-03 10:35:43 -0700

    add function list within Game class

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -9,10 +9,77 @@ public class Game {
 
   public Game(int pPlayerCount) {
     this.playerCount = pPlayerCount;
-    for (int i = ; i < 8; i+=2) {
-
+    for (int i = 0; i < 8; i++) {
+      for(int j = i%2; j < 8; j+=2) {
+        if (i <=2)
+          board[i][j] = 1;
+        else if (i >= 3 && i <= 4)
+          board[i][j] = 0;
+        else
+          board[i][j] = 2;
+      }
     }
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+  /// get and Methods
+
+  public int getId() {
+    return this.id;
+  }
+
+  public int getPlayerCount() {
+
+  }
+
+  public List<Integer> getUserIds() {
+
+  }
+
+  public List<User> getUsers() {
+
+  }
+
+  public int[][] getBoard() {
+
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  /// validator Methods
+
+  public boolean generalMoveIsAvailable(int pRow, int pCol) {
+
+  }
+
+  public boolean generalCaptureIsAvailable(int pRow, int pCol) {
+
+  }
+
+  public boolean legalMove(int pRowStart, int pColStart, int pRowFinish, int pColFinish) {
+
+  }
+
+  public boolean specificMoveIsAvailable(int pRowStart, int pColStart, int pRowFinish, int pColFinish) {
+
+  }
+
+  public boolean specificCaptureIsAvailable(int pRowStart, int pColStart, int pRowFinish, int pColFinish) {
+
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  /// gamePlay Methods
+
+  public void movePiece(int pRowStart, int pColStart, int pRowFinish, int pColFinish) {
+
+  }
+
+  public void capturePiece(int pRowStart, int pColStart, int pRowFinish, int pColFinish) {
+
+  }
+
+  public void makeKing(int pRow, int pCol) {
+
+  }
 
 }
