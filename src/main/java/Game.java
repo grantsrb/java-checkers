9b6cdb8afd6d2774fe67f04e7ce933cc99d0f20e
Chance Neff and Satchel Grant <chance+satchel@>
2016-10-03 14:26:52 -0700

    add legal move validators

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,10 +1,12 @@
 import org.sql2o.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class Game {
   private int playerCount;
   private int playerTurn;
+  private List<Checker> checkers;
   private int id;
 
   public Game(int pPlayerCount) {
@@ -13,12 +15,16 @@ public class Game {
     this.save();
     for (int i = 0; i < 8; i++) {
       for(int j = i%2; j < 8; j+=2) {
-        if (i <=2)
+        if (i <=2) {
           Checker newChecker = new Checker(1, i, j, this.id);
-        else if (i >= 3 && i <= 4)
+          checkers.add(newChecker);
+        } else if (i >= 3 && i <= 4) {
           Checker newChecker = new Checker(0, i, j, this.id);
-        else
+          checkers.add(newChecker);
+        } else {
           Checker newChecker = new Checker(2, i, j, this.id);
+          checkers.add(newChecker);
+        }
       }
     }
   }
@@ -34,71 +40,114 @@ public class Game {
     return this.playerCount;
   }
 
-//TODO sandro and caleb
-  public List<Integer> getUserIds() {
+  public int getPlayerTurn() {
+    return this.playerTurn;
+  }
 
+  public List<Integer> getUserIds() {
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery("SELECT userId FROM users_games WHERE gameId=:gameId")
+        .addParameter("gameId", this.id)
+        .executeAndFetch(Integer.class);
+    }
   }
 
   public List<User> getUsers() {
-
+    List<User> users = new ArrayList<>();
+    for (Integer userId : this.getUserIds())
+      users.add(User.findById(userId));
+    return users;
   }
 
-  //====
-
-  public int[][] getBoard() {
-    return this.board;
-  }
-
-  /////////////////////////////////////////////////////////////////////////////
-  /// database managament Methods
-
-  //TODO Compare boards
-  @Override
-  public boolean equals(Object otherGame) {
-    if(!(otherGame instanceof Game)) {
-      return false;
-    } else {
-      Game newGame = (Game) otherGame;
-      return this.id == newGame.getId() &&
-             this.playerCount == newGame.getPlayerCount();
+  public List<Checker> getCheckers() {
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery("SELECT * FROM checkers WHERE gameId=:gameId")
+        .addParameter("gameId", this.id)
+        .executeAndFetch(Checker.class);
     }
   }
 
   /////////////////////////////////////////////////////////////////////////////
   /// validator Methods
 
-  public boolean generalMoveIsAvailable(int pRow, int pCol) {
+  public boolean isLegal(Checker pChecker, int pRowFinish, int pColFinish) {
+    if(pRowFinish < 0 || pRowFinish > 7 || pColFinish > 7 || pColFinish < 0) {
+      return false;
+    } else if(pChecker.getColumnPosition() == pColFinish || pRowFinish == pChecker.getRowPosition()) {
+      return false;
+    }else if(pChecker.getType() == 1 && pRowFinish < pChecker.getRowPosition()) {
+      return false;
+    } else if (pChecker.getType() == 2 && pRowFinish > pChecker.getRowPosition()) {
+      return false;
+    } else {
+      for (int i = 0; i < 8; i++) {
+        for(int j = (i+1)%2; j < 8; j+=2) {
+          if (pRowFinish == i && pColFinish == j)
+            return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  public boolean isLegalMove(Checker pChecker, int pRowFinish, int pColFinish) {
+    if (!this.isLegal(pChecker, pRowFinish, pColFinish)) {
+      return false;
+    } else if (1 < Math.abs(pChecker.getRowPosition() - pRowFinish)) {
+      return false;
+    } else if (1 < Math.abs(pChecker.getColumnPosition() - pColFinish)) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isLegalCapture(Checker pChecker, int pRowFinish, int pColFinish) {
+    if (!this.isLegal(pChecker, pRowFinish, pColFinish)) {
+      return false;
+    } else if (1 == Math.abs(pChecker.getRowPosition() - pRowFinish) || 2 < Math.abs(pChecker.getRowPosition() - pRowFinish)) {
+      return false;
+    } else if (1 == Math.abs(pChecker.getColumnPosition() - pColFinish) || 2 < Math.abs(pChecker.getColumnPosition() - pColFinish)) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean specificMoveIsAvailable(Checker pChecker, int pRowFinish, int pColFinish) {
+    if (!this.isLegalMove(pChecker, pRowFinish, pColFinish))
+      return false;
+    for (Checker checker : this.checkers) {
+      if (checker.getRowPosition == pRowFinish && checker.getColumnPosition() == pColFinish)
+        return false;
+    }
+    return true;
+  }
+
+  public boolean specificCaptureIsAvailable(Checker pChecker, int pRowFinish, int pColFinish) {
 
   }
 
-  public boolean generalCaptureIsAvailable(int pRow, int pCol) {
+  public boolean generalCaptureIsAvailable(Checker pChecker) {
 
   }
 
-  public boolean legalMove(int pRowStart, int pColStart, int pRowFinish, int pColFinish) {
+  public boolean generalMoveIsAvailable(Checker pChecker) {
 
   }
 
-  public boolean specificMoveIsAvailable(int pRowStart, int pColStart, int pRowFinish, int pColFinish) {
 
-  }
 
-  public boolean specificCaptureIsAvailable(int pRowStart, int pColStart, int pRowFinish, int pColFinish) {
 
-  }
+
+
 
   ///////////////////////////////////////////////////////////////////////////
   // gamePlay Methods
 
-  public void movePiece(int pRowStart, int pColStart, int pRowFinish, int pColFinish) {
+  public void movePiece(Checker pChecker, int pRowFinish, int pColFinish) {
 
   }
 
-  public void capturePiece(int pRowStart, int pColStart, int pRowFinish, int pColFinish) {
-
-  }
-
-  public void makeKing(int pRow, int pCol) {
+  public void capturePiece(Checker pChecker, int pRowFinish, int pColFinish) {
 
   }
 
@@ -120,6 +169,17 @@ public class Game {
       con.createQuery("DELETE FROM games WHERE id=:id")
         .addParameter("id", this.id)
         .executeUpdate();
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherGame) {
+    if(!(otherGame instanceof Game)) {
+      return false;
+    } else {
+      Game newGame = (Game) otherGame;
+      return this.id == newGame.getId() &&
+             this.playerCount == newGame.getPlayerCount() && this.playerTurn == newGame.getPlayerTurn();
     }
   }
 
