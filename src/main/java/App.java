c9a78e83f5bd3c1f41a4de9724ade566da3b9cde
Chance Neff and Satchel Grant <satchel@>
2016-10-04 09:59:18 -0700

    add sign in for front end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,12 +14,71 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<>();
-      int[] board = {0,1,2,3,4,5,6,7};
-      model.put("rows", board);
-      model.put("columns", board);
+      model.put("loggedInStatus", User.loggedIn);
+      model.put("loggedInUser", User.loggedInUser);
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/checkers-game", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      int[] board = {0,1,2,3,4,5,6,7};
+      model.put("rows", board);
+      model.put("columns", board);
+      model.put("template", "templates/checkers.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/login",(request,response) -> { // Directs to sign in page
+      Map<String,Object> model = new HashMap<>();
+      return new ModelAndView(model, "templates/login.vtl");
+    },new VelocityTemplateEngine());
+
+    post("/login", (request,response) -> { // Directs to home page if successful login
+      Map<String,Object> model = new HashMap<>();
+      String userName = request.queryParams("user-name");
+      String password = request.queryParams("password");
+      try {
+        User newUser = User.login(userName,password);
+        User.loggedIn = true;
+        User.loggedInUser = newUser;
+        model.put("loggedInStatus", User.loggedIn);
+        model.put("loggedInUser", User.loggedInUser);
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+      } catch(RuntimeException exception) { }
+      model.put("invalidLogin", true);
+      return new ModelAndView(model, "templates/login.vtl");
+    },new VelocityTemplateEngine());
+
+    post("/new-account", (request,response) -> {
+      Map<String,Object> model = new HashMap<>();
+      String userName = request.queryParams("create-user-name");
+      String password = request.queryParams("create-password");
+      if (!User.userAlreadyExists(userName)) { // If user name is valid, directs to home page
+        User.loggedIn = true;
+        User newUser = new User(userName, password);
+        newUser.save();
+        User.loggedInUser = userName;
+        model.put("loggedInStatus", User.loggedIn);
+        model.put("loggedInUser", User.loggedInUser);
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+      } else {
+        model.put("invalidUserName", true);
+        return new ModelAndView(model, "templates/login.vtl");
+      }
+    },new VelocityTemplateEngine());
+
+    post("/logout", (request, response) -> {
+      Map<String,Object> model = new HashMap<>();
+      User.loggedIn = false;
+      User.loggedInUser = null;
+      model.put("loggedInStatus", User.loggedIn);
+      model.put("loggedInUser", User.loggedInUser);
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    },new VelocityTemplateEngine());
 
     // get("/", (request, response) -> {
     //   Map<String, Object> model = HashMap<>();
